9fde98047f4c193f46298761ed224360b8243fbf
David Chaney <david.ray.chaney@gmail.com>
2023-06-29 08:47:43 -0500

    update projects page

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,6 +1,3 @@
-// import strengths from '../assets/5-strengths.png'
-// import hobbies from '../assets/hobbies.png'
-// import about from '../assets/about.png'
 import david from '../assets/david.jpg'
 import camp from '../assets/camp.jpg'
 import croatia from '../assets/croatia.jpg'
@@ -14,26 +11,27 @@ export default function Bio(){
       <h1>Bio</h1>
       <div className="text-center"><img src={david} alt="david"/></div>
       <p>Hi, I'm David, an experienced people leader, process manager, and software developer based in Tulsa, OK.</p>
-      <p>And I'm good at it.  I've managed multi-million dollar projects, lead large teams, and am frequently celebrated for winning results.</p>
-      <p>Most of my experience is in telecommunications & supply chain.  My non-leader experience is in networks, RF, and coding.  The only exception was a brief few years working construction when I was in college.</p>
+      <p>And I'm good at it!  I've managed multi-million dollar projects, lead large teams, and achieved celebrated results!</p>
+      <p>Most of my leadership experience is in telecommunications & supply chain, and my technical experience is in networks, RF, and coding.</p>
+      <p>I also worked a few years of residential and commercial construction when I was in college.</p>
       <figure className="figure">
         <img src={strengths} alt="strengths" className="figure-img img-fluid rounded"/>
         <figcaption className="figure-caption text-end">StrengthFinder Strengths</figcaption>
       </figure>
-      <p>With strengths like these, it's easy to see that I enjoy reading, repairing, and building.  Throughout my career, I've frequently tasked peers and direct reports to find their strengths and explore their personalities.  To this end, I've always been on high-performing teams because we always know and respect the talents and abilities of one another.</p>
+      <p>With strengths like these, it's easy to see that I enjoy reading, repairing, and building.  Throughout my career, I've frequently asked peers and direct reports to find their strengths and explore their personalities.  To this end, I've always been on high-performing teams because we always know and respect the talents and abilities of one another.</p>
       <p>My strengths, combined with my experience, has given me an exceptional talent for fixing what's broke and making good things great. And I enjoy it, too!</p>
       <figure className="figure">
         <img src={fixing} alt="Fixing" className="figure-img img-fluid rounded"/>
         <figcaption className="figure-caption text-end">That's David!</figcaption>
       </figure>
       <p>Personally, I have been coding for many years, and have experience in PC/Mac/Linux/AS400.  I enjoy all-things-geek, so along with programming, I also have experience with cyber security, networking, and circuit boards.</p>
-      <p>Outside of tech, I enjoy getting outside!  Dispersed camping``, off-roading, cycling, hiking, gardening, and traveling - sunlight fuels me!</p>
+      <p>Outside of tech, I enjoy getting outside!  Dispersed camping, off-roading rugged trails, cycling, hiking, gardening, and traveling - sunlight fuels me!</p>
       <figure className="figure">
         <img src={camp} alt="camping" className="figure-img img-fluid rounded"/>
         <figcaption className="figure-caption text-end">Dispersed camping, Colorado</figcaption>
       </figure>
       <p>I also enjoy making music, art, and going on family adventures, big & small.</p>
-      <p>Family adventures have become a priority.  I'm <a href="https://www.smithfamilymortuaries.com/memorials/jennifer-chaney/3940103/obit.php?&printable=true">widowed with two sons</a>, remarried in 2022, and moved to Tulsa where I gained two bonus step kids.  I love exploring and traveling with my family.</p>
+      <p>Family adventures have become a priority.  I'm <a className="link-this" href="https://www.smithfamilymortuaries.com/memorials/jennifer-chaney/3940103/obit.php?&printable=true">widowed with two sons</a>, remarried in 2022, and moved to Tulsa where I gained two bonus step kids.  I love exploring and traveling with my family.</p>
       <figure className="figure">
         <img src={croatia} alt="croatia" className="figure-img img-fluid rounded"/>
         <figcaption className="figure-caption text-end">Exploring Croatia</figcaption>
@@ -43,29 +41,7 @@ export default function Bio(){
         <img src={hforh} alt="roofing" className="figure-img img-fluid rounded"/>
         <figcaption className="figure-caption text-end">Building roofs with Habitat for Humanity</figcaption>
       </figure>
-      <p>I don't cook amazingly well, I can't sleep in past 6AM, and I can't concurrently maintain more than one social media presence, so feel free to connect with me on the only social app I maintain by pressing the LinkedIn icon below!</p>
-      
-
-      {/* <div className="toc text-center">
-        <h4>Contents:</h4>
-        <ul>
-          <li><a href="#strengths">Top Strengths</a></li>
-          <li><a href="#hobbies">Hobbies</a></li>
-          <li><a href="#about">About</a></li>
-        </ul>
-      </div>
-
-      <div className="text-center">
-        <img id="strengths" className="bio-images" src={strengths} alt="top strengths"/>
-      </div>  
-      
-      <div className="text-center">
-        <img id="hobbies" className="bio-images" src={hobbies} alt="my hobbies"/>
-      </div>
-
-      <div className="text-center">
-        <img id="about" className="bio-images" src={about} alt="about me"/>
-      </div>  */}
+      <p>I have many strengths, but not among them is maintaining multiple social media apps, so feel free to connect with me on <a className="link-this" href="https://www.linkedin.com/in/drchaney/">LinkedIn</a>!</p>
     </>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,150 +3,123 @@ import connectfour from "../assets/connect-four.png"
 import ppp from "../assets/ppp.png"
 import fitnesstrackr from "../assets/fitnesstrackr.png"
 import Video from "../assets/sembrar.mp4"
+import projects from "../assets/projects.png"
 
 export default function Projects(){
   return(
     <>
-      <h1>Projects</h1>
-
-      <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed focus-ring focus-ring-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-            JavaScript Projects
+      <div className="text-center"><a href="https://github.com/drchaney"><img src={projects} alt="project title" className="figure-img img-fluid rounded"/></a></div>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed focus-ring focus-ring-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+              JavaScript Projects
             </button>
           </h2>
-          <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-            <div className="my-3 container text-center">
-        <img className="project-image" src={spoonbrain} alt="spoonbrain screenshot"/>
-      </div>
-      <table className="table table-sm">
-        <tr>
-          <td><h6>Project: </h6></td>
-          <td><p><a href="https://davids-guessing-game.netlify.app/">Spoonbrain ESP Implants</a></p></td>
-        </tr>
-        <tr>
-          <td><h6>For: </h6></td>
-          <td><p>Class</p></td>
-        </tr>
-        <tr>
-          <td><h6>Description: </h6></td>
-          <td><p>The project was to demonstrate our recently acquired HTML5, CSS, and JavaScript skills by creating a guessing game.  The player would have 5 attempts to guess one through 100, and could get a hint if needed.</p></td>
-        </tr>
-        <tr>
-          <td><h6>Reason for Portfolio: </h6></td>
-          <td><p>I had a blast on this project!  I used JavaScript to change CSS variables so that the player would have a "heatmap" that would calculate how "hot" or "cold" their guess was and display the corresponding color the more accurate their guess.  I also demonstrated flex box but was nonetheless unhappy with my design skills, so I created a 2nd version of this project to learn Bootstrap, thus creating a much cleaner look.</p></td>
-        </tr>
-      </table>
-      <hr/>
-      <div className="my-3 container text-center">
-        <img className="project-image" src={connectfour} alt="connect four screenshot"/>
-      </div>
-      <table className="table table-sm">
-        <tr>
-          <td><h6>Project: </h6></td>
-          <td><p><a href="https://4-connect.netlify.app/">Four Connect</a></p></td>
-        </tr>
-        <tr>
-          <td><h6>For: </h6></td>
-          <td><p>Class</p></td>
-        </tr>
-        <tr>
-          <td><h6>Description: </h6></td>
-          <td><p>This was our "final exam" for the Fundamentals portion of our class, to show off our HTML/CSS/JavaScript abilities by picking a game and coding it from scratch.</p></td>
-        </tr>
-        <tr>
-          <td><h6>Reason for Portfolio: </h6></td>
-          <td><p>I had recently read an article on mobile-first development, so even though the project didn't call for mobile phone usability, I still developed this primarily for my phone. I also ready many articles on CSS animation because it wasn't taught in class and I felt that could make the game go from good to great.  In addition to the animation, I was proud of the AI logic, and the massive object and object methods I created to keep the game moving forward.  My only pain was trying to animate a checker "falling", but it always clipped the game board, so I settled on a spinning animation to place the checkers.</p></td>
-        </tr>
-      </table>
+          <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <div className="my-3 container text-center">
+                <img className="project-image" src={spoonbrain} alt="spoonbrain screenshot"/>
+              </div>
+              <table className="table table-sm">
+                <tr>
+                  <td><h6>Project: </h6></td>
+                  <td><p><a href="https://davids-guessing-game.netlify.app/">Spoonbrain ESP Implants</a></p></td>
+                </tr>
+                <tr>
+                  <td><h6>Description: </h6></td>
+                  <td><p>A guessing game project demonstrating HTML5/CSS/JavaScript working together in harmony, with a Bootstrap framework bonus.</p></td>
+                </tr>
+              </table>
+              <hr/>
+              <div className="my-3 container text-center">
+                <img className="project-image" src={connectfour} alt="connect four screenshot"/>
+              </div>
+              <table className="table table-sm">
+                <tr>
+                  <td><h6>Project: </h6></td>
+                  <td><p><a href="https://4-connect.netlify.app/">Four Connect</a></p></td>
+                </tr>
+                <tr>
+                  <td><h6>Description: </h6></td>
+                  <td><p>Responsive CSS with an AI JavaScript opponent, this game can be played on both small and large screens.</p></td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed focus-ring focus-ring-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Frontend & React
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed focus-ring focus-ring-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Frontend & React Projects
             </button>
           </h2>
-          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-            <div className="my-3 container text-center">
-        <img className="project-image" src={ppp} alt="peculiar peoples possessions screenshot"/>
-      </div>
-      <table className="table table-sm">
-        <tr>
-          <td><h6>Project: </h6></td>
-          <td><p><a href="https://peculiar-peoples-possessions.netlify.app/">Peculiar Peoples Possessions</a></p></td>
-        </tr>
-        <tr>
-          <td><h6>For: </h6></td>
-          <td><p>Class</p></td>
-        </tr>
-        <tr>
-          <td><h6>Description: </h6></td>
-          <td><p>This was our "final exam" for the Frontend portion of our class, to demonstrate our JSON, JWT, AJAX, API, and React abilities by creating a "Craigslist-Lite" web application.</p></td>
-        </tr>
-        <tr>
-          <td><h6>Reason for Portfolio: </h6></td>
-          <td><p>This project was a ton of fun, and I enjoyed quickly creating a React project that was dynamic.  I was proud of figuring out how to make a responsive search bar that would update the results with each key pressed, based on matches in the posting's title, description, location, and username, instead of waiting for the "Search" button to be clicked.</p></td>
-        </tr>
-      </table>
-      <hr/>
-      <div className="my-3 container text-center">
-        <img className="project-image" src={fitnesstrackr} alt="fitness trackr screenshot"/>
-      </div>
-      <table className="table table-sm">
-        <tr>
-          <td><h6>Project: </h6></td>
-          <td><p>Fitness Track <a href="https://github.com/fitness-track/fitness-tracker-frontend">Frontend</a> & <a href="https://github.com/fitness-track/fitness-track">Backend</a></p></td>
-        </tr>
-        <tr>
-          <td><h6>For: </h6></td>
-          <td><p>Class</p></td>
-        </tr>
-        <tr>
-          <td><h6>Description: </h6></td>
-          <td><p>This was our "final exam" for the Backend portion of our class, to demonstrate Express and PostgreSQL abilities by creating a web application to manage accounts and exercise routines.</p></td>
-        </tr>
-        <tr>
-          <td><h6>Reason for Portfolio: </h6></td>
-          <td><p>This project was my first time working in github, rotating Scrum Master duties, creating tickets, submitting pull requests, and communicating daily in virtual stand-ups with a team!</p></td>
-        </tr>
-      </table>
+          <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <div className="my-3 container text-center">
+                <img className="project-image" src={ppp} alt="peculiar peoples possessions screenshot"/>
+              </div>
+              <table className="table table-sm">
+                <tr>
+                  <td><h6>Project: </h6></td>
+                  <td><p><a href="https://peculiar-peoples-possessions.netlify.app/">Peculiar Peoples Possessions</a></p></td>
+                </tr>
+                <tr>
+                  <td><h6>Description: </h6></td>
+                  <td><p>A Craiglist clone to demonstrate JSON, JWT, AJAX, fetching APIs, and React.</p></td>
+                </tr>
+              </table>
+              <hr/>
+              <div className="my-3 container text-center">
+                <img className="project-image" src={fitnesstrackr} alt="fitness trackr screenshot"/>
+              </div>
+              <table className="table table-sm">
+                <tr>
+                  <td><h6>Project: </h6></td>
+                  <td><p>Fitness Track <a href="https://github.com/fitness-track/fitness-tracker-frontend">Frontend</a></p></td>
+                </tr>
+                <tr>
+                  <td><h6>Description: </h6></td>
+                  <td><p>A fitness tracking application created with a team via scrum, demonstrating group work, tickets, pull requests, React, and APIs.</p></td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed focus-ring focus-ring-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              Backend & APIs
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed focus-ring focus-ring-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Backend Projects & APIs
             </button>
           </h2>
-          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-            <div className="my-3 container text-center">
-      <video className="project-image project-video" src={Video} playsInline autoPlay loop muted></video>
-      </div>
-      <table className="table table-sm">
-        <tr>
-          <td><h6>Project: </h6></td>
-          <td><p><a href="https://github.com/drchaney/sembrar">Sembrar</a></p></td>
-        </tr>
-        <tr>
-          <td><h6>For: </h6></td>
-          <td><p>Class</p></td>
-        </tr>
-        <tr>
-          <td><h6>Description: </h6></td>
-          <td><p>This was our "final exam!"  Goal: make a fully functional, yet fictional, full stack eCommerce application.  The "client" requests to have customers login, or check out as guest, have their carts follow them across devices (logged in users) or at least be restored with the browser (for guest users), and manage products.</p></td>
-        </tr>
-        <tr>
-          <td><h6>Reason for Portfolio: </h6></td>
-          <td><p>This final project was a ton of fun, from creating user stories, wireframes, database diagrams, to execution.  I enjoyed adding "extras", like more than one image per product, the ability to edit your cart by mousing over it, having stories, articles, promo codes, and sales campaigns managed through an admin portal.  I exceeded the course requirements, but didn't finish all the user stories I created in the four weeks we had to work on this.  Nonetheless, I learned a ton from the many, many hours I poured into this project.</p></td>
-        </tr>
-      </table>
+          <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <div className="my-3 container text-center">
+                <img className="project-image" src={fitnesstrackr} alt="fitness trackr screenshot"/>
+              </div>
+              <table className="table table-sm">
+                <tr>
+                  <td><h6>Project: </h6></td>
+                  <td><p>Fitness Track <a href="https://github.com/fitness-track/fitness-track">Backend</a></p></td>
+                </tr>
+                <tr>
+                  <td><h6>Description: </h6></td>
+                  <td><p>Node, Express, and PostgreSQL web application to manage accounts and exercise routines created by users.</p></td>
+                </tr>
+              </table>
+              <div className="my-3 container text-center">
+                <video className="project-image project-video" src={Video} playsInline autoPlay loop muted></video>
+              </div>
+              <table className="table table-sm">
+                <tr>
+                  <td><h6>Project: </h6></td>
+                  <td><p><a href="https://github.com/drchaney/sembrar">Sembrar</a></p></td>
+                </tr>
+                <tr>
+                  <td><h6>Description: </h6></td>
+                  <td><p>An e-commerce application that I made with React, I completed this project by coding a fully comprehensive backend to handle inventory, carts, users, reviews, and checking out. </p></td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
